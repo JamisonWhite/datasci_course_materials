04d3620b6c8e8613ad7841bd3f145a5d680c4835
JamisonWhite <jwsk03!GH>
2013-05-08 13:12:52 -0500

    added normalize functions for tweet and word

--- assignment1/frequency.py
+++ assignment1/frequency.py
@@ -3,11 +3,36 @@ frequency.py -- Calculates term frequency
 """
 __author__ = 'Jamison White'
 
-import json
 import sys
+import json
+import re
 from collections import defaultdict
 
-def read_tweet_text(tweet_filename):
+
+def get_file_names(tweets='../../data/output.txt', sentiments='../../data/AFINN-111.txt'):
+    """
+    get_file_names -- gets file names based on assignment 1 instructions.
+    I think the order is backwards and that tweet file should have come first.
+    """
+    if len(sys.argv) >= 3:
+        sentiments = sys.argv[1]
+        tweets = sys.argv[2]
+    elif len(sys.argv) >= 2:
+        tweets = sys.argv[1]
+    return tweets, sentiments
+
+
+def normalize_word(word):
+    return word
+    # return word.lower()
+    #return re.sub('[^a-z0-9 -]', '', word.lower()) #from hjort
+
+
+def normalize_tweet(tweet):
+    return tweet.encode('utf-8')
+
+
+def read_tweet_text(tweet_filename, normalize=normalize_tweet):
     """
     read_tweets -- generator reads tweet json from file with one tweet per line
     """
@@ -16,38 +41,36 @@ def read_tweet_text(tweet_filename):
             tweet_json = json.loads(line)
             if 'text' in tweet_json:
                 text = tweet_json['text']
-                yield text.encode('utf-8')
+                yield normalize(text)
 
 
-def term_frequency_boolean(tweets):
+def term_frequency_count(tweets, normalize=normalize_word):
     tf = defaultdict(int)
     for tweet in tweets:
         for word in tweet.split():
-            tf[word] += 1
+            tf[normalize(word)] += 1
     return tf
 
 
-def term_frequency_percentage(tweets):
-    tfb = term_frequency_boolean(tweets)
+def term_frequency_percentage(tweets, normalize=normalize_word):
+    tfb = term_frequency_count(tweets, normalize)
     total_terms = sum(tfb.values())
     tf = {k: float(v) / total_terms for k, v in tfb.iteritems()}
     return tf
 
 
 def main():
-    #get the args
-    if len(sys.argv) >= 2:
-        tweet_filename = sys.argv[1]
-    else:
-        tweet_filename = '../../data/output.txt'
+
+    tweets, sentiments = get_file_names(tweets='C:\Data\TwitterStream\output.txt')
 
     #calculate term frequency
-    terms = term_frequency_percentage(read_tweet_text(tweet_filename))
+    terms = term_frequency_percentage(read_tweet_text(tweets))
 
     #print results sorted
-    for k in sorted(terms, key=terms.get, reverse=True):
+    for k in sorted(terms, key=terms.get, reverse=True)[:100]:
         print "{0} {1:.10f}".format(k, terms[k])
 
+    print "{0} terms".format(len(terms))
 
 if __name__ == '__main__':
     main()
