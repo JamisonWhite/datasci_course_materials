7040f3f5cb012e6908b95140045dce10b4df84d2
JamisonWhite <jwsk03!GH>
2013-05-14 16:38:44 -0500

    Finished assignment 2

--- assignment2/problem_2_3.sql
+++ assignment2/problem_2_3.sql
@@ -7,12 +7,39 @@ join Frequency as f2 on f1.term = f2.term
 where f1.docid = '10080_txt_crude'
 and f2.docid = '17035_txt_earn';
 
--- similarity
+-- similarity between two docs; homework answer is 19
 select sum(f1.count * f2.count)
 from Frequency as f1 
 join Frequency as f2 on f1.term = f2.term
 where f1.docid = '10080_txt_crude'
 and f2.docid = '17035_txt_earn';
+
+-- 10 most similiar documents
+select f2.docid, sum(f1.count * f2.count)
+from Frequency as f1 
+join Frequency as f2 on f1.term = f2.term
+where f1.docid = '10080_txt_crude'
+group by f2.docid	
+order by sum(f1.count * f2.count) desc
+limit 10;
+
+
+-- 10 most similiar documents; homework answer is 6
+select f2.docid, sum(f1.count * f2.count)
+from 
+(select * from Frequency 
+ union
+ select 'q' as docid, 'washington' as term, 1 as count
+ union
+ select 'q' as docid, 'taxes' as term, 1 as count
+ union
+ select 'q' as docid, 'treasury' as term, 1 as count
+) as f1 
+join Frequency as f2 on f1.term = f2.term
+where f1.docid = 'q'
+group by f2.docid	
+order by sum(f1.count * f2.count) desc
+limit 10;
 
 
 -- multiplication
