3804e2458998a3c2c1d92eaa10f358e1b0948d7f
JamisonWhite <robertjamisonwhite@gmail.com>
2013-05-22 22:29:48 -0500

    working on matrix multiply completed one pass unable to get two pass working with current class

--- assignment3/multiply.py
+++ assignment3/multiply.py
@@ -3,51 +3,92 @@ __author__ = 'jamie'
 
 import sys
 import MapReduce
-from collections import defaultdict
+import json
 
 # Part 1
 mr = MapReduce.MapReduce()
+mr2 = MapReduce.MapReduce()
 
 
 # Part 2
 def mapper(record):
 
-    matrix = record[0]
-    x = record[1]
-    y = record[2]
-    value = record[3]
+    #Relation M(I, J, V) with tuples (m, i, j, m_ij)
+    #Relation N(J, K, W) with tuples (n, j, k, n_jk)
 
-    if matrix == 'a':
-        mr.emit_intermediate('arow{0}'.format(x), ('a', x, y, value))
-        mr.emit_intermediate('acol{0}'.format(y), ('a', x, y, value))
+    k = 4
 
-        mr.emit_intermediate('brow{0}'.format(y), ('a', x, y, value))
-        mr.emit_intermediate('bcol{0}'.format(x), ('a', x, y, value))
-
-    if matrix == 'b':
-        mr.emit_intermediate('arow{0}'.format(y), ('b', x, y, value))
-        mr.emit_intermediate('acol{0}'.format(x), ('b', x, y, value))
-
-        mr.emit_intermediate('brow{0}'.format(x), ('b', x, y, value))
-        mr.emit_intermediate('bcol{0}'.format(y), ('b', x, y, value))
+    if record[0] == 'a':
+        M = record[0]
+        i = record[1]
+        j = record[2]
+        v = record[3]
+        for k in range(5):
+            mr.emit_intermediate((i, k), [M, j, v])
 
 
-    # for i in range(x):
-    #     for j in range(y):
-    #         k = '{0},{1}'.format(i, j)
-    #         p = (j if matrix=='a' else i)
-    #         v = [matrix, (i,j), value]
-    #         mr.emit_intermediate(k, v)
+    if record[0] == 'b':
+        N = record[0]
+        j = record[1]
+        k = record[2]
+        w = record[3]
+        for i in range(5):
+            mr.emit_intermediate((i, k), [N, j, w])
 
-# select sum(a.value * b.value)
-# from a
-# join b on a.row_num = b.col_num or a.col_num = b.row_num
-# where a.row_num = 2 and b.col_num = 3
-# group by a.row_num, b.col_num;
 
 # Part 3
-def reducer(key, list_of_values):
+def reducer_writer(key, list_of_values):
     mr.emit((key, list_of_values))
+
+def reducer(key, list_of_values):
+    M = {}
+    N = {}
+    for v in list_of_values:
+        if v[0] == 'a':
+            M[v[1]] = v[2]
+        else:
+            N[v[1]] = v[2]
+
+    total = 0
+    J = set.intersection(set(M.keys()), set(N.keys()))
+    for j in J:
+        total += M[j] * N[j]
+    result = list(key)
+    result.append(total)
+    mr.emit(result)
+
+
+# Part 2
+def mapper_step1(record):
+
+    #Relation M(I, J, V) with tuples (m, i, j, m_ij)
+    #Relation N(J, K, W) with tuples (n, j, k, n_jk)
+
+    if record[0] == 'a':
+        mr.emit_intermediate(record[2], (record[0], record[1], record[3]))
+
+    if record[0] == 'b':
+        mr.emit_intermediate(record[1], (record[0], record[2], record[3]))
+
+
+
+def reducer_step1(key, list_of_values):
+    #mr.emit((key, list_of_values))
+
+    M = [v for v in list_of_values if v[0] == 'a']
+    N = [v for v in list_of_values if v[0] == 'b']
+
+    for m in M:
+        for n in N:
+            mr.emit([m[1], n[1], m[2] *  n[2]])
+
+
+def mapper_step2(record):
+    mr2.emit_intermediate([record[0], record[1]], record[2])
+
+
+def reducer_step2(key, list_of_values):
+    mr2.emit([key, sum(list_of_values)])
 
 
 # Part 4
@@ -59,6 +100,7 @@ def main():
 
     with open(data, 'r') as f:
         mr.execute(f, mapper, reducer)
+
 
 
 if __name__ == '__main__':
